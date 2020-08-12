6983ce022951ffcec1cba5d34245d299d1a4ff78
yoursshahnawaz <shahnawazgalaxy340@gmail.com>
2020-08-12 17:41:58 +0530

    added socket.io & few validations

--- src/index.js
+++ src/index.js
@@ -1,14 +1,45 @@
+const http = require('http')
 const express = require('express')
 const path = require('path')
+const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const port  = process.env.PORT || 3000;
-
 const publicDirectory = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectory))
 
-app.listen(3000, () => {
+io.on('connection', (socket) => {
+    console.log('New WebSocket Connection!');
+
+    socket.emit('message', 'Welcome!')
+    socket.broadcast.emit('message', 'A new user has joined!')
+
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter
+
+        if(filter.isProfane(message)) {
+            return callback('Bad words are not allowed!')
+        }
+
+        io.emit('message', message)
+        callback()
+    })
+
+    socket.on('sendLocation', (coords, callback) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!')
+    })
+})
+
+server.listen(3000, () => {
     console.log('Server is running on ' + port)
 })
