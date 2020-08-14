a6c55556b8a7fedcd811a26afa671d2dae0a4c6e
yoursshahnawaz <shahnawazgalaxy340@gmail.com>
2020-08-14 14:00:11 +0530

    sending messages to rooms

--- public/js/chat.js
+++ public/js/chat.js
@@ -16,6 +16,7 @@ const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true }
 socket.on('message', (message) => {
     console.log(message);
     const html = Mustache.render(messageTemplate, {
+        username: message.username,
         message: message.text,
         createdAt: moment(message.createdAt).format('hh:mm a')
     })
@@ -25,6 +26,7 @@ socket.on('message', (message) => {
 socket.on('locationMessage', (message) => {
     console.log(message);
     const html = Mustache.render(locationTemplate, {
+        username: message.username,
         url: message.url,
         createdAt: moment(message.createdAt).format('hh:mm a')
     })
@@ -67,4 +69,9 @@ $sendLocationButton.addEventListener('click', () => {
     })
 })
 
-socket.emit('join', { username, room })
+socket.emit('join', { username, room }, (error) => {
+    if(error) {
+        alert(error)
+        location.href = '/'
+    }
+})
