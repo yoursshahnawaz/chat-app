217008c986fb20502460c93b982966dc53e07426
yoursshahnawaz <shahnawazgalaxy340@gmail.com>
2020-08-14 12:44:12 +0530

    added chat rooms

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,6 +10,9 @@ const $messages = document.querySelector('#messages')
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationTemplate = document.querySelector('#location-template').innerHTML
 
+// Options
+const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
+
 socket.on('message', (message) => {
     console.log(message);
     const html = Mustache.render(messageTemplate, {
@@ -63,3 +66,5 @@ $sendLocationButton.addEventListener('click', () => {
         })
     })
 })
+
+socket.emit('join', { username, room })
--- src/index.js
+++ src/index.js
@@ -17,8 +17,12 @@ app.use(express.static(publicDirectory))
 io.on('connection', (socket) => {
     console.log('New WebSocket Connection!');
 
-    socket.emit('message', generateMessage('Welcome!'))
-    socket.broadcast.emit('message', generateMessage('A new user has joined!'))
+    socket.on('join', ({ username, room }) => {
+        socket.join(room)
+
+        socket.emit('message', generateMessage('Welcome!'))
+        socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined!`))
+    })
 
     socket.on('sendMessage', (message, callback) => {
         const filter = new Filter
